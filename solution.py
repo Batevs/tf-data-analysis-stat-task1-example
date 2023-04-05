15bdbce0fc6e31595b6968ff52794dc95411e663
Batevs <129524449+Batevs@users.noreply.github.com>
2023-04-05 12:12:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5273335974 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
+    t = 62.0
+    t_squared = t ** 2
+    solutions = (2 * x) / t_squared
+        # Измените код этой функции
+    # Это будет вашим решениеsolutionsм
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return x.mean(solutions) # Ваш ответ
